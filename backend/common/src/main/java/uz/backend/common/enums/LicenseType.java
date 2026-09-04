bc9f56b46889b48dbf07dcc6471be976c5cd74d9
Александр <@DESKTOP-L9V170T>
2026-09-04 17:36:05 +0500

    Add manager aggregate

---
+++ backend/common/src/main/java/uz/backend/common/enums/LicenseType.java
@@ -0,0 +1,23 @@
+package uz.backend.common.enums;
+
+/**
+ * Типы лицензий
+ *
+ * @author Aleksandr Yagudin
+ */
+public enum LicenseType {
+    /**
+     * Управление общежитиями
+     */
+    DORMITORY,
+
+    /**
+     * Организация питания
+     */
+    CATERING,
+
+    /**
+     * Техническое обслуживание
+     */
+    MAINTENANCE
+}
